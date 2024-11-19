ed932a9dcaadb0844444aebcc456d8df95017993
Andreas Linus Thalund Midtgaard <andreaslinus@gmail.com>
2024-11-19 14:51:37 +0100

    alt nu i stukker

---
+++ src/views/about.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h2>Test side 2</h2>
+    </div>
+</template>
---
+++ src/views/home.vue
@@ -0,0 +1,17 @@
+<script>
+import datafetch from './components/datafetch.vue';
+
+export default {
+  components: {
+    datafetch, // Register the component with this name
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <h1>User List</h1>
+    <datafetch /> <!-- Use the correct name here -->
+    
+  </div>
+</template>
