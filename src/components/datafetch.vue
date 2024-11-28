efca836637dd96c5a01952f9ea896ed095485d03
Andreas Linus Thalund Midtgaard <andreaslinus@gmail.com>
2024-11-28 10:36:22 +0100

    NNN

--- src/components/datafetch.vue
+++ src/components/datafetch.vue
@@ -18,6 +18,10 @@
 </template>
 
 <script>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
 export default {
   data() {
     return {
