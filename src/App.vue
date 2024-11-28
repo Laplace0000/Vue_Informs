745689020e79719eea4fb3cb194853c2409133d8
Andreas Linus Thalund Midtgaard <andreaslinus@gmail.com>
2024-11-28 10:13:48 +0100

    expanding tables using primevue

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,13 @@
 </template>
 
 <script>
+import Button from "primevue/button"
+
 export default {
   name: "App",
 };
+app.component('Button', Button);
+
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import PrimeVue from 'primevue/config';
+import aura from '@primevue/themes/aura';
+
 
 const app = createApp(App); // Create the app instance
 app.use(router); 
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 app.mount('#app');
