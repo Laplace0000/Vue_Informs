b07fd77763964e0ede903488f34ec0baeb33fefb
Andreas Linus Thalund Midtgaard <andreaslinus@gmail.com>
2024-12-10 18:03:31 +0100

    trying but unsuccesfull in getting delete to work

--- src/Methods/tableFun.js
+++ src/Methods/tableFun.js
@@ -9,84 +9,14 @@ export function hideDialog() {
     submitted.value = false;
 }
 
-export function saveProduct() {
-    submitted.value = true;
 
-    if (product?.value.name?.trim()) {
-        if (product.value.id) {
-            product.value.inventoryStatus = product.value.inventoryStatus.value ? product.value.inventoryStatus.value : product.value.inventoryStatus;
-            products.value[findIndexById(product.value.id)] = product.value;
-            toast.add({severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000});
-        } else {
-            users.value.id = createId();
-            users.value.code = createId();
-            users.value.image = 'product-placeholder.svg';
-            users.value.push(product.value);
-            toast.add({severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000});
-        }
+export function deleteObjects(objects){
+    console.log('deleting objects', objects);
 
-        productDialog.value = false;
-        product.value = {};
-    }
-}
-
-export function editProduct(prod) {
-    users.value = { ...prod };
-    productDialog.value = true;
-}
-
-export function confirmDeleteProduct(prod) {
-    product.value = prod;
-    deleteDialog.value = true;
-}
-
-export function deleteProduct() {
-    users.value = users.value.filter(val => val.id !== product.value.id);
-    deleteDialog.value = false;
-    product.value = {};
-    toast.add({severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000});
-}
-
-export function findIndexById(id) {
-    let index = -1;
-    for (let i = 0; i < users.value.length; i++) {
-        if (users.value[i].id === id) {
-            index = i;
-            break;
-        }
-    }
-    return index;
-}
-
-export function createId() {
-    let id = '';
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < 5; i++) {
-        id += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return id;
 }
 
 export function exportCSV(dt) {
     dt.exportCSV();
-}
-
-export function confirmDeleteSelected() {
-    deleteDialog.value = true;
-}
-
-export function deleteSelected() {
-    users.value = products.value.filter(
-        val => !selectedProducts.value.includes(val)
-    );
-    deleteDialog.value = false;
-    selectedProducts.value = null;
-    toast.add({
-        severity: 'success',
-        summary: 'Successful',
-        detail: 'Products Deleted',
-        life: 3000
-    });
 }
 
 export function handleJsonUpload(event) {
--- src/components/makeUserTable.vue
+++ src/components/makeUserTable.vue
@@ -2,7 +2,6 @@
 import { DataTable, Column, FileUpload, Toolbar, Button, InputText } from 'primevue';
 import { FilterMatchMode } from '@primevue/core/api';
 import { ref } from 'vue'; 
-import { onMounted } from 'vue';
 import * as tableFun from '../Methods/tableFun';
 
 const { userData } = defineProps(['userData'])
@@ -17,58 +16,76 @@ const filters = ref({
 });
 </script>
 
-<template>          <Button label="Export" icon="pi pi-upload" severity="secondary" @click="tableFun.exportCSV(dt.value)" />
+<template>         
 
-  <div class="card">
+  <div>
+    <div class="card">
+      <Toolbar class="mb-6">
+        <template #start>
+            <Button 
+              label="New" 
+              icon="pi pi-plus" 
+              class="mr-2" 
+              @click="openNew" />
+            <Button 
+              label="Delete" 
+              icon="pi pi-trash" 
+              severity="danger" 
+              outlined @click="tableFun.deleteObjects()" 
+              :disabled="!selectedProducts || !selectedProducts.length" />
+        </template>
 
-    <Toolbar class="mb-6">
-      <template #start>
-          <Button label="New" icon="pi pi-plus" class="mr-2" @click="openNew" />
-          <Button label="Delete" icon="pi pi-trash" severity="danger" outlined @click="tableFun.confirmDeleteSelected" :disabled="!selectedProducts || !selectedProducts.length" />
-      </template>
+        <template #end>
+            <FileUpload 
+            mode="basic" 
+            accept="application/json" 
+            :maxFileSize="1000000" 
+            label="Import" 
+            customUpload 
+            chooseLabel="Import" 
+            class="mr-2" 
+            auto 
+            :chooseButtonProps="{ severity: 'secondary' }" />
+            <Button 
+            label="Export" 
+            icon="pi pi-upload" 
+            severity="secondary" 
+            @click="tableFun.exportCSV(dt)" />
+        </template>
+      </Toolbar>
 
-      <template #end>
-          <FileUpload mode="basic" accept="application/json" :maxFileSize="1000000" label="Import" customUpload chooseLabel="Import" class="mr-2" auto :chooseButtonProps="{ severity: 'secondary' }" />
-          <Button label="Export" icon="pi pi-upload" severity="secondary" @click="tableFun.exportCSV(dt)" />
-      </template>
-    </Toolbar>
-
-    <div class="flex justify-between mb-2">
-      <InputText v-model="filters['global'].value" placeholder="Search Entries..." />
+      <div class="flex justify-between mb-2">
+        <InputText v-model="filters['global'].value" placeholder="Search Entries..." />
+      </div>
+      <DataTable
+        ref="dt"
+        :value="userData"
+        :filters="filters"
+        :globalFilterFields="columns.map(col => col.field)"
+        v-model:selection="selectedProducts"
+        selectionMode="multiple" 
+        stripedRows
+        sortMode="multiple"
+        removableSort
+        paginator
+        :rows="5"
+        tableStyle="min-width: 50rem"
+      >
+      <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+      <Column field ="id" header = "ID" sortable  ></Column>
+      <Column field ="name" header = "Name" sortable  ></Column>
+      <Column field ="email" header = "Email" sortable  ></Column>
+      </DataTable>        
+      <Column :exportable="false" style="min-width: 12rem">
+        <template #body="slotProps">
+            <Button icon="pi pi-pencil" outlined rounded class="mr-2" @click="editProduct(slotProps.data)" />
+            <Button icon="pi pi-trash" outlined rounded severity="danger" @click="confirmDeleteProduct(slotProps.data)" />
+        </template>
+      </Column>
     </div>
-    <DataTable
-      ref="dt"
-      :value="userData"
-      :filters="filters"
-      :globalFilterFields="columns.map(col => col.field)"
-      :selection="selectedProduct"
-      stripedRows
-      sortMode="multiple"
-      removableSort
-      paginator
-      :rows="5"
-      tableStyle="min-width: 50rem"
-    >
-    <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
-    <Column field ="id" header = "ID" sortable  ></Column>
-    <Column field ="name" header = "Name" sortable  ></Column>
-    <Column field ="email" header = "Email" sortable  ></Column>
-    </DataTable>        
-    <Column :exportable="false" style="min-width: 12rem">
-      <template #body="slotProps">
-          <Button icon="pi pi-pencil" outlined rounded class="mr-2" @click="editProduct(slotProps.data)" />
-          <Button icon="pi pi-trash" outlined rounded severity="danger" @click="confirmDeleteProduct(slotProps.data)" />
-      </template>
-    </Column>
   </div>
-  <Dialog v-model:visible="deleteDialog" :style="{ width: '450px' }" header="Confirm" :modal="true">
-    <div class="flex items-center gap-4">
-      <i class="pi pi-exclamation-triangle !text-3xl" />
-      <span v-if="user">Are you sure you want to delete user?</span>
-    </div>
-    <template #footer>
-      <Button label="No" icon="pi pi-times" text @click="deleteDialog = false" />
-      <Button label="Yes" icon="pi pi-check" text @click="deleteSelected" />
-    </template>
-  </Dialog>
+
+
+
+
 </template>
