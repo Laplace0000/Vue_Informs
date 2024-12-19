d6c0863befa34684765ab90d92a3d2de3e43b3dc
Andreas Linus Thalund Midtgaard <andreaslinus@gmail.com>
2024-12-19 17:13:50 +0100

    fjernet ubrugt json handler funciton

--- src/Methods/tableFun.js
+++ src/Methods/tableFun.js
@@ -115,39 +115,6 @@ export function exportCSV(objects) {
     objects.exportCSV();
 }
 
-export function handleJsonUpload(event) {
-    const file = event.files[0];
-
-    if (file) {
-        const reader = new FileReader();
-
-        reader.onload = function(e) {
-            try {
-                const jsonData = JSON.parse(e.target.result);
-                console.log("Uploaded JSON Data:", jsonData);
-
-                // Process the JSON data
-                products.value = [...products.value, ...jsonData];
-                toast.add({
-                    severity: 'success',
-                    summary: 'Successful',
-                    detail: 'JSON data uploaded successfully',
-                    life: 3000,
-                });
-            } catch (error) {
-                console.error("Error parsing JSON file:", error);
-                toast.add({
-                    severity: 'error',
-                    summary: 'Error',
-                    detail: 'Invalid JSON file',
-                    life: 3000,
-                });
-            }
-        };
-        reader.readAsText(file);
-    }
-}
-
 
 
 // Table-related functions
